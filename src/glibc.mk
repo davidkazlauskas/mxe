e161d0df10dc77841c630e2f232ac526d6760a00
David <david@templatious.org>
2015-11-25 17:03:50 +0200

    boom

--- src/glibc.mk
+++ src/glibc.mk
@@ -4,7 +4,7 @@
 PKG             := glibc
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.22
-$(PKG)_CHECKSUM := 96cd9711d8f38fa6f99af085a67ad1e0ebca339f2a9a00a2aa59c40a66c4552d
+$(PKG)_CHECKSUM := eb731406903befef1d8f878a46be75ef862b9056ab0cde1626d08a7a05328948
 $(PKG)_SUBDIR   := glibc-$($(PKG)_VERSION)
 $(PKG)_FILE     := glibc-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://ftp.gnu.org/gnu/glibc/$($(PKG)_FILE)
