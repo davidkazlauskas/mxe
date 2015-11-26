c4aa86c285aecae3254ab6d992c26c0f4f8153b4
David <david@templatious.org>
2015-11-26 21:31:29 +0200

    porting samples, like a boss

--- src/jpeg-test.c
+++ src/jpeg-test.c
@@ -3,20 +3,20 @@
  * See index.html for further information.
  */
 
+#include <stdint.h>
 #include <stdio.h>
-#include <windows.h>
 #include <jpeglib.h>
 
 int main(int argc, char *argv[])
 {
-    boolean test_boolean;
-    INT32 test_int32;
+    int test_boolean;
+    int32_t test_int32;
     struct jpeg_decompress_struct cinfo;
 
     (void)argc;
     (void)argv;
 
-    test_boolean = TRUE;
+    test_boolean = 1;
     test_int32 = 1;
     (void)test_boolean;
     (void)test_int32;
