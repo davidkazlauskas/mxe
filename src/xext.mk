57239fb7eac927c95d14ec96c4aca1a15928e788
David <david@templatious.org>
2015-11-30 21:31:56 +0200

    xext

---
+++ src/xext.mk
@@ -0,0 +1,23 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := xext
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.3.1
+$(PKG)_CHECKSUM := 56229c617eb7bfd6dec40d2805bc4dfb883dfe80f130d99b9a2beb632165e859
+$(PKG)_SUBDIR   := libXext-$($(PKG)_VERSION)
+$(PKG)_FILE     := libXext-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := http://www.x.org/releases/X11R7.7/src/everything/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://curl.haxx.se/download/?C=M;O=D' | \
+    $(SED) -n 's,.*curl-\([0-9][^"]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        $(MXE_CONFIGURE_OPTS)
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+endef
