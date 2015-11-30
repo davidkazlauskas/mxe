2cf4199ee7a59978451c80c9f9de6fb859bc3c7b
David <david@templatious.org>
2015-11-30 11:10:37 +0200

    xaulib + xcb compiled

--- src/xaulib.mk
+++ src/xaulib.mk
@@ -1,14 +1,14 @@
 # This file is part of MXE.
 # See index.html for further information.
 
-PKG             := xcblib
+PKG             := xaulib
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.8.1
-$(PKG)_CHECKSUM := d2f46811e950710e7e79e45615d24f2c7ec318b9de9dc717972723da58bffa0d
-$(PKG)_SUBDIR   := libxcb-$($(PKG)_VERSION)
-$(PKG)_FILE     := libxcb-$($(PKG)_VERSION).tar.bz2
-$(PKG)_URL      := http://www.x.org/releases/X11R7.7/src/xcb/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc xcbproto pthread-stubs xaulib
+$(PKG)_VERSION  := 1.0.7
+$(PKG)_CHECKSUM := 7153ba503e2362d552612d9dc2e7d7ad3106d5055e310a26ecf28addf471a489
+$(PKG)_SUBDIR   := libXau-$($(PKG)_VERSION)
+$(PKG)_FILE     := libXau-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := http://www.x.org/releases/X11R7.7/src/everything/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://curl.haxx.se/download/?C=M;O=D' | \
