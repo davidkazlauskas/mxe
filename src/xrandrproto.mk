cf4a57081b27b19d250a2731c778a14dea39ec21
David <david@templatious.org>
2015-12-01 12:26:33 +0200

    xrandr build

--- src/xrandrproto.mk
+++ src/xrandrproto.mk
@@ -1,14 +1,14 @@
 # This file is part of MXE.
 # See index.html for further information.
 
-PKG             := xrandr
+PKG             := xrandrproto
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.3.2
-$(PKG)_CHECKSUM := 7eaca216ab5233d7396119eb87c1989d350a3efead104d54b55f22cdd1d99b81
-$(PKG)_SUBDIR   := libXrandr-$($(PKG)_VERSION)
-$(PKG)_FILE     := libXrandr-$($(PKG)_VERSION).tar.bz2
+$(PKG)_CHECKSUM := d90d00612cc49292bd8d9dc19efb1c9187385fbe87590d7279a02e5e1066dc71
+$(PKG)_SUBDIR   := randrproto-$($(PKG)_VERSION)
+$(PKG)_FILE     := randrproto-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.x.org/releases/X11R7.7/src/everything/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc xrandrproto
+$(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://curl.haxx.se/download/?C=M;O=D' | \
