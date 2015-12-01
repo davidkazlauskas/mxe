34a45384ef85e0513ced6af35088595ad2b11b73
David <david@templatious.org>
2015-12-01 12:19:40 +0200

    xinput with 1 dep

---
+++ src/xi.mk
@@ -0,0 +1,23 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := xi
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.6.1
+$(PKG)_CHECKSUM := f2e3627d7292ec5eff488ab58867fba14a62f06e72a8d3337ab6222c09873109
+$(PKG)_SUBDIR   := libXi-$($(PKG)_VERSION)
+$(PKG)_FILE     := libXi-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := http://www.x.org/releases/X11R7.7/src/everything/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://curl.haxx.se/download/?C=M;O=D' | \
+    $(SED) -n 's,.*curl-\([0-9][^"]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        $(MXE_CONFIGURE_OPTS)
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+endef
---
+++ src/xinput.mk
@@ -0,0 +1,23 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := xinput
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.6.0
+$(PKG)_CHECKSUM := 4ab007d952c76665603bcb82ceb15fd3929d10faf0580fc4873ac16f5f63847e
+$(PKG)_SUBDIR   := xinput-$($(PKG)_VERSION)
+$(PKG)_FILE     := xinput-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := http://www.x.org/releases/X11R7.7/src/everything/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc xi
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://curl.haxx.se/download/?C=M;O=D' | \
+    $(SED) -n 's,.*curl-\([0-9][^"]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        $(MXE_CONFIGURE_OPTS)
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+endef
