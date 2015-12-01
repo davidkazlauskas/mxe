cc28cd8b39fe97dd3e96aeec839e327820d63074
David <david@templatious.org>
2015-12-01 13:14:32 +0200

    xtst builds

--- src/xrecordproto.mk
+++ src/xrecordproto.mk
@@ -1,14 +1,14 @@
 # This file is part of MXE.
 # See index.html for further information.
 
-PKG             := xtst
+PKG             := xrecordproto
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.2.1
-$(PKG)_CHECKSUM := 7eea3e66e392aca3f9dad6238198753c28e1c32fa4903cbb7739607a2504e5e0
-$(PKG)_SUBDIR   := libXtst-$($(PKG)_VERSION)
-$(PKG)_FILE     := libXtst-$($(PKG)_VERSION).tar.bz2
+$(PKG)_VERSION  := 1.14.2
+$(PKG)_CHECKSUM := a777548d2e92aa259f1528de3c4a36d15e07a4650d0976573a8e2ff5437e7370
+$(PKG)_SUBDIR   := recordproto-$($(PKG)_VERSION)
+$(PKG)_FILE     := recordproto-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.x.org/releases/X11R7.7/src/everything/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc xrecordproto
+$(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://curl.haxx.se/download/?C=M;O=D' | \
