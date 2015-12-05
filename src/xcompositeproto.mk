678e5d90028dc7ecee2c02dac2b6d116be314bac
David <david@templatious.org>
2015-12-05 09:36:47 +0200

    xcomp proto

--- src/xcompositeproto.mk
+++ src/xcompositeproto.mk
@@ -1,14 +1,14 @@
 # This file is part of MXE.
 # See index.html for further information.
 
-PKG             := xcomposite
+PKG             := xcompositeproto
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.4.3
-$(PKG)_CHECKSUM := 32294d28f4ee46db310c344546d98484728b7d52158c6d7c25bba02563b41aad
-$(PKG)_SUBDIR   := libXcomposite-$($(PKG)_VERSION)
-$(PKG)_FILE     := libXcomposite-$($(PKG)_VERSION).tar.bz2
+$(PKG)_VERSION  := 0.4.2
+$(PKG)_CHECKSUM := 049359f0be0b2b984a8149c966dd04e8c58e6eade2a4a309cf1126635ccd0cfc
+$(PKG)_SUBDIR   := compositeproto-$($(PKG)_VERSION)
+$(PKG)_FILE     := compositeproto-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.x.org/releases/X11R7.7/src/everything/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc xlib xcompositeproto
+$(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://curl.haxx.se/download/?C=M;O=D' | \
@@ -19,7 +19,7 @@ endef
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
-        --enable-shared \
-        --disable-static
+        CFLAGS="$(CFLAGS) -fPIC" \
+        CXXFLAGS="$(CXXFLAGS) -fPIC"
     $(MAKE) -C '$(1)' -j '$(JOBS)' install
 endef
