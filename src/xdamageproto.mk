1cefa461130813d4e4dba63fc4f3bdd97232d196
David <david@templatious.org>
2015-12-06 09:53:49 +0200

    damage

--- src/xdamageproto.mk
+++ src/xdamageproto.mk
@@ -1,14 +1,14 @@
 # This file is part of MXE.
 # See index.html for further information.
 
-PKG             := xdamage
+PKG             := xdamageproto
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.1.3
-$(PKG)_CHECKSUM := bc6169c826d3cb17435ca84e1b479d65e4b51df1e48bbc3ec39a9cabf842c7a8
-$(PKG)_SUBDIR   := libXdamage-$($(PKG)_VERSION)
-$(PKG)_FILE     := libXdamage-$($(PKG)_VERSION).tar.bz2
+$(PKG)_VERSION  := 1.2.1
+$(PKG)_CHECKSUM := 5c7c112e9b9ea8a9d5b019e5f17d481ae20f766cb7a4648360e7c1b46fc9fc5b
+$(PKG)_SUBDIR   := damageproto-$($(PKG)_VERSION)
+$(PKG)_FILE     := damageproto-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.x.org/releases/X11R7.7/src/everything/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc xdamageproto
+$(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://curl.haxx.se/download/?C=M;O=D' | \
@@ -19,7 +19,7 @@ endef
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
-        --enable-shared \
-        --disable-static
+        CFLAGS="$(CFLAGS) -fPIC" \
+        CXXFLAGS="$(CXXFLAGS) -fPIC"
     $(MAKE) -C '$(1)' -j '$(JOBS)' install
 endef
